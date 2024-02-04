3f475c4fa35fce13c78bc1e8d01dded6339255dd
Abhinav <raghunandan.abhinav@gmail.com>
2024-02-04 23:27:58 +0530

    Add validations for form input fields

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,14 @@
 class Activity < ApplicationRecord
-    belongs_to :user
+    belongs_to :user   
+  
+    # Adding validations to activity fields  
+    validates :title, presence: true, length: { maximum: 100 }
+    validates :activity_type, presence: true, length: { maximum: 50 }
+    validates :start, presence: true
+
+    # Duration and Calories should be an integer value (Cannot be negative)
+    validates :duration, presence: true, numericality: { only_integer: true, greater_than: 0 }
+    validates :calories, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  
 end
